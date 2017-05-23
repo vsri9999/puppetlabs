0fa38aad0c7b32451e9835f514cd801f594698eb
vsri9999 <srirama.vaitala@gmail.com>
2017-05-23 14:40:55 +0530

    test

--- environments/production/manifests/site.pp
+++ environments/production/manifests/site.pp
@@ -1,5 +1,21 @@
+node 'node1' {
+       include firstmodule
+       include lint
+       include users::user21
+       include users::user22
+       include group::groups
+       include webservice::package
+}
+node default {
+}
 file{ 'motd':
      ensure => present,
      path => '/etc/motd',
      content => 'This is to test',
+}
+
+file{'status':
+     ensure => present,
+     path => '/root/first.txt',
+     content => "Hardware : ${facts['os']['hardware']}" ,
 }
